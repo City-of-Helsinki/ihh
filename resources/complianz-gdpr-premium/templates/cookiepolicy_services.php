968621991b3c9ae2d2780ea334c430b66e41474c
Kimmo Thai <kimmo.thai@ambientia.fi>
2022-05-13 17:10:39 +0300

    WIP complianz styles

---
+++ resources/complianz-gdpr-premium/templates/cookiepolicy_services.php
@@ -0,0 +1,20 @@
+<details class="cmplz-dropdown cmplz-service-desc cmplz-dropdown-cookiepolicy ">
+	<summary class="cmplz-service-header">
+		<span class="marker has-icon"><?php helsinki_svg_icon('angle-down'); ?></span>
+		<span class="cmplz-title-wrap cmplz-service-title"><h3>{service}</h3></span>
+		<span class="purpose" >{allPurposes}</span>
+	</summary>
+    <div class="cmplz-service-description">
+	    <span class="cmplz-title-wrap cmplz-description-title">
+				<h4><?php _ex("Usage", 'cookie policy', 'complianz-gdpr') ?></h4>
+			</span>
+      <p>{purposeDescription}</p>
+    </div>
+    <div class="cmplz-sharing-data">
+        <span class="cmplz-title-wrap cmplz-sharing-title">
+					<h4><?php _ex("Sharing data", 'Legal document cookie policy', 'complianz-gdpr') ?></h4>
+				</span>
+        <p>{sharing}</p>
+    </div>
+    {cookies}
+</details>
