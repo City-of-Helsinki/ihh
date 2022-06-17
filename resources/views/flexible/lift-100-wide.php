d92f0ecf85d074c341b61f1e14aaf661e829cd2e
Kimmo Thai <kimmo.thai@ambientia.fi>
2022-06-17 16:27:58 +0300

    Rest of the layout, styling

--- resources/views/flexible/lift-100-wide.php
+++ resources/views/flexible/lift-100-wide.php
@@ -11,23 +11,26 @@
         $bg_color = $colors[$color_selection];
     }
     $bg_color_style = sprintf("style=background-color:%s;", $bg_color);
+
+    $arrow_right = '<svg class="hds-arrow-right" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><rect width="24" height="24"></rect><polygon fill="currentColor" points="10.5 5.5 12 7 8 11 20.5 11 20.5 13 8 13 12 17 10.5 18.5 4 12" transform="matrix(-1 0 0 1 24.5 0)"></polygon></g></svg>';
 ?>
 
 <div class="container">
-    <div class="lift-100-wide lift-100-wide--<?php the_sub_field("side"); ?> w-100 h-100">
-        <!-- <div class="lift-100-wide__bg w-100" ></div> -->
-        <img class="lift-100-wide__bg-img w-75 h-100 img-fluid" src="<?php the_sub_field("background_image");?>">
-        <div class="lift-100-wide__card w-50 p-4 pb-5" <?php echo $bg_color_style; ?> >
-            <h3 class="lift-100-wide__title"><?php the_sub_field("title"); ?></h3>
-            <p class="lift-100-wide__text"><?php the_sub_field("text_body"); ?></p>
-            <div class="lift-100-wide__links">
-                <ul>
-                <?php foreach (get_sub_field("links") as $row) {
-                    $link = $row["link"];
-                    $text = $row["link_text"];
-                    echo "<li><a href=$link>Icon placeholder</a>$text</li>";
-                } ?>
-                </ul>
+    <div class="lift-100-wide lift-100-wide--<?php the_sub_field("side"); ?> w-100 h-100 my-5">
+        <div class="row">
+            <img class="lift-100-wide__bg-img w-75 h-100" src="<?php the_sub_field("background_image");?>">
+            <div class="lift-100-wide__card w-50 p-5" <?php echo $bg_color_style; ?> >
+                <h3 class="lift-100-wide__title"><?php the_sub_field("title"); ?></h3>
+                <p class="lift-100-wide__text"><?php the_sub_field("text_body"); ?></p>
+                <div class="lift-100-wide__links">
+                    <ul class="m-0 p-0">
+                    <?php foreach (get_sub_field("links") as $row) {
+                        $link = $row["link"];
+                        $text = $row["link_text"];
+                        echo "<li class='my-2'><a href=$link>$arrow_right</a>$text</li>";
+                    } ?>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
