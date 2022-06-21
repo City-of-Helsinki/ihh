33ad6e3633c097b18d648a0ffa89355b37b761c0
Mikko Paasto <mikko.paasto@ambientia.fi>
2022-06-21 11:16:40 +0300

    Add arrow svg icon automatically also for internal links using class

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,5 +1,7 @@
 export default {
   init() {
+    const ARROW_SVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.6 32.1" xml:space="preserve" role="presentation"><path style="fill-rule:evenodd;clip-rule:evenodd" d="M32.6 16.1 16.5 0l-2.8 2.8 11.4 11.3H0v4h25L13.7 29.3l2.8 2.8z"></path></svg>';
+
     $('.venobox').venobox({
         spinner: 'wave',
         bgcolor: '#000',
@@ -95,37 +97,46 @@ export default {
     })
 
 
-    function detectExternalLinks(item){
+    function detectExternalLinks(item) {
         const hostName = window.location.hostname;
         const links = document.querySelectorAll(item);
 
         for (let i = 0; i < links.length; i++) {
             let domain = (new URL(links[i]));
-            domain = domain.hostname.replace('www.','');
-            console.log(hostName + ' / ' + domain);
-            if(hostName !== domain){
+            domain = domain.hostname.replace('www.', '');
+
+            if (hostName !== domain) {
                 addExternalLinkStyling(links[i]);
             } else {
-                console.log('ok');
+                if (links[i].classList.contains('arrow')) {
+                    addInternalLinkIcon(links[i]);
+                }
             }
         }
     }
 
-    function addExternalLinkStyling(link){
+    function addExternalLinkStyling(link) {
         link.classList.add('external-link');
         addExternalLinkIcon(link);
     }
 
-    function addExternalLinkIcon(link){
-        const svgArrow45 = '<span class="inline-svg rotate-45"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.6 32.1" xml:space="preserve" role="presentation"><path style="fill-rule:evenodd;clip-rule:evenodd" d="M32.6 16.1 16.5 0l-2.8 2.8 11.4 11.3H0v4h25L13.7 29.3l2.8 2.8z"></path></svg></span>';
+    function addExternalLinkIcon(link) {
+        const svgArrow45 = '<span class="inline-svg rotate-45">'+ARROW_SVG+'</span>';
         const inlineSVG = link.querySelector('.inline-svg');
 
-        if( inlineSVG === null ){
+        if (inlineSVG === null) {
             jQuery(link).prepend(svgArrow45);
         }
     }
 
+    function addInternalLinkIcon(link) {
+        const svgArrow45 = '<span class="inline-svg rotate-0">'+ARROW_SVG+'</span>';
+        const inlineSVG = link.querySelector('.inline-svg');
 
+        if (inlineSVG === null) {
+            jQuery(link).prepend(svgArrow45);
+        }
+    }
   },
   finalize() {},
 };
