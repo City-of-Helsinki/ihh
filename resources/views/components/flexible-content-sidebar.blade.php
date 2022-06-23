c5b7e3dd30ce2950782d47433863d1919c8ff11e
Toni Helvilä <toni.helvila@ambientia.fi>
2022-06-23 15:41:36 +0300

    HELOSK-168, Yksittäisen palvelun sivu: general information and guidance -nosto sivupalkille

---
+++ resources/views/components/flexible-content-sidebar.blade.php
@@ -0,0 +1,3 @@
+<section>
+    @php do_action('ihh_render_flexible_content', 'sidebar_components' ); @endphp
+</section>
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,10 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
-  <article class="content-block container">
-    @include('partials.content.header')
-    @include('partials.content.page')
-  </article>
-  @endwhile
+@while(have_posts()) @php the_post() @endphp
+    <div class="content-block container single-page-layout">
+        <div class="row">
+            <div class="col-md-8">
+                <article class="content-block">
+                    @include('partials.content.header')
+                    @include('partials.content.page')
+
+                    @include('components.employers-flexible-content')
+                </article>
+            </div>
+
+            <div class="col-lg-4 sidebar">
+                @include('components.navigate-pages')
+                @include('components.flexible-content-sidebar')
+            </div>
+        </div>
+
+        <div class="row content-footer">
+            <div class="col-lg-12">
+                @include('components.navigate-pages')
+            </div>
+        </div>
+    </div>
+    @endwhile
 @endsection
