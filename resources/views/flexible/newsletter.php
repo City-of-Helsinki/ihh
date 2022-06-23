5477b4871318bc6bd70e8f2e3214756d193cf468
Kimmo Thai <kimmo.thai@ambientia.fi>
2022-06-23 16:07:58 +0300

    Radio buttons to row

--- resources/views/flexible/newsletter.php
+++ resources/views/flexible/newsletter.php
@@ -6,14 +6,14 @@
       <label for="newsletterEmail" class="form-label">Email*</label>
       <input name="email" type="email" class="form-control border border-dark" required></input>
     </div>
-    <div class="mb-3 radio-buttons">
-      <div class="form-check target-group-employers">
+    <div class="mb-3 radio-buttons row mx-0">
+      <div class="form-check target-group-employers col-6">
         <input class="form-check-input" type="radio" name="flexRadio" id="flexRadioEmployers">
         <label class="form-check-label" for="flexRadioEmployers">
           Newsletter for employers
         </label>
       </div>
-      <div class="form-check target-group-newcomers">
+      <div class="form-check target-group-newcomers col-6">
         <input class="form-check-input" type="radio" name="flexRadio" id="flexRadioNewcomers">
         <label class="form-check-label" for="flexRadioNewcomers">
           Newsletter for newcomers
@@ -22,7 +22,7 @@
     </div>
     <div class="mb-3 privacy-policy form-check">
       <input type="checkbox" class="form-check-input" id="privacyPolicyCheck">
-      <label class="form-check-label" for="privacyPolicyCheck">I agree that my details can be processed according to the <a href="https://www.ambientia.fi">Privacy Policy</a></label>
+      <label class="form-check-label" for="privacyPolicyCheck">I agree that my details can be processed according to the <a href="#">Privacy Policy</a></label>
     </div>
     <div class="mb-3">
       <button type="submit" class="btn rounded-pill subscribe px-5">Subscribe</button>
