9384f9c7835a514f899969efd1bc22e21bdf7dae
Kimmo Thai <kimmo.thai@ambientia.fi>
2022-07-26 14:15:41 +0300

    Add ACF fields for subscription success and failure links

--- resources/views/flexible/newsletter.php
+++ resources/views/flexible/newsletter.php
@@ -8,8 +8,8 @@
       </div>
       <p><?php the_sub_field("newsletter_body_text");?></p>
       <form method="post" action="<?php the_sub_field("liana_subscription_page"); ?>" class="lianamailer" id="lianamailer">
-        <input type="hidden" name="success_url" value="<?php echo get_home_url(); ?>">
-        <input type="hidden" name="failure_url" value="<?php echo get_home_url(); ?>">
+        <input type="hidden" name="success_url" value="<?php echo get_sub_field("liana_success_page_url"); ?>">
+        <input type="hidden" name="failure_url" value="<?php echo get_sub_field("liana_failure_page_url"); ?>">
         <div class="mb-3 email">
           <label for="newsletterEmail" class="form-label">Email*</label>
           <input name="email" type="email" class="form-control border border-dark" required></input>
@@ -33,7 +33,7 @@
         </div>
         <div class="mb-3 privacy-policy form-check">
           <input required type="checkbox" class="form-check-input" id="privacyPolicyCheck">
-          <label class="form-check-label" for="privacyPolicyCheck">I agree that my details can be processed according to the <a href="#">Privacy Policy</a></label>
+          <label class="form-check-label" for="privacyPolicyCheck">I agree that my details can be processed according to the <a href="<?php echo get_privacy_policy_url(); ?>">Privacy Policy</a></label>
         </div>
         <div class="mb-3 d-flex justify-content-center">
           <button type="submit" class="btn rounded-pill subscribe px-5">Subscribe</button>
