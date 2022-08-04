50e8a113cde4087799604d560ab4a5f4ead2a551
Toni Helvilä <toni.helvila@ambientia.fi>
2022-08-04 13:08:57 +0300

    HELOSK-162, Icon fixes

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -39,7 +39,7 @@ export default {
         const anchorNavigation = document.querySelector(navigationEl);
         const headings = document.querySelectorAll(anchorTargetEl);
         const svgArrowRight =
-            '<span class="inline-svg"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path fill="#000" d="M13 4v12.5l4-4 1.5 1.5-6.5 6.5L5.5 14 7 12.5l4 4V4z"/></g></svg></span>';
+            '<span class="inline-svg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.6 32.1" xml:space="preserve" role="presentation"><path style="fill-rule:evenodd;clip-rule:evenodd" d="M32.6 16.1 16.5 0l-2.8 2.8 11.4 11.3H0v4h25L13.7 29.3l2.8 2.8z"></path></svg></span>';
 
         if (headings && anchorNavigation) {
             let ul = document.createElement('ul');
