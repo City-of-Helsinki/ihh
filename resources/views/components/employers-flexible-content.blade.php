6644153506a98896da11235846bb21067e2c175b
Kimmo Thai <kimmo.thai@ambientia.fi>
2022-08-05 14:28:22 +0300

    Rename ACF contact-5050 to support PHP syntax, layout implementation

--- resources/views/components/employers-flexible-content.blade.php
+++ resources/views/components/employers-flexible-content.blade.php
@@ -1,3 +1,4 @@
 <section>
-    @php do_action('ihh_render_flexible_content', 'components' ); @endphp
+    <!-- @php do_action('ihh_render_flexible_content', 'components' ); @endphp -->
+    @php do_action('ihh_render_flexible_content', 'lift_100_wide' ); @endphp
 </section>
