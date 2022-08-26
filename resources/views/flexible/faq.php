22d51bed6cfeb9d08b16a4007feecd26f47152a7
Toni Helvilä <toni.helvila@ambientia.fi>
2022-08-26 14:22:19 +0300

    HELOSK-176, Style updates for desktop and mobile devices

--- resources/views/flexible/faq.php
+++ resources/views/flexible/faq.php
@@ -1,33 +1,30 @@
 <div class="container" id="faqs">
-    <div class="col">
     <?php if( get_sub_field('section_heading')): ?>
         <h2 class="section-heading" id="section-heading-<?php echo get_the_ID(); ?>"><?php the_sub_field('section_heading'); ?></h2>
     <?php endif; ?>
     </div>
-    <div class="col">
-        <?php if( have_rows('faq_accordion') ) : ?>
-            <?php while( have_rows('faq_accordion') ) : the_row(); $id = get_row_index(); ?>
-                <div class="question accordion">
-                    <button class="question-header collapsed"
-                        data-toggle="collapse"
-                        data-target="#accordion_content_<?php echo $id; ?>"
-                        aria-expanded="false"
-                        aria-controls="accordion_content_<?php echo $id; ?>"
-                        aria-owns="accordion_content_<?php echo $id; ?>">
-                        <span><?php the_sub_field('question'); ?></span>
-                    </button>
 
-                    <div id="accordion_content_<?php echo $id; ?>"
-                        class="collapse accordion-content"
-                        data-parent="#faqs">
+    <?php if( have_rows('faq_accordion') ) : ?>
+        <?php while( have_rows('faq_accordion') ) : the_row(); $id = get_row_index(); ?>
+            <div class="question accordion">
+                <button class="question-header collapsed"
+                    data-toggle="collapse"
+                    data-target="#accordion_content_<?php echo $id; ?>"
+                    aria-expanded="false"
+                    aria-controls="accordion_content_<?php echo $id; ?>"
+                    aria-owns="accordion_content_<?php echo $id; ?>">
+                    <span><?php the_sub_field('question'); ?></span>
+                </button>
 
-                        <div class="accordion-body">
-                            <?php echo get_sub_field('answer'); ?>
-                        </div>
+                <div id="accordion_content_<?php echo $id; ?>"
+                    class="collapse accordion-content"
+                    data-parent="#faqs">
 
+                    <div class="accordion-body">
+                        <?php echo get_sub_field('answer'); ?>
                     </div>
                 </div>
-            <?php endwhile; ?>
-        <?php endif; ?>
-    </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
 </div>
