6cc1eccbd12311b0ea91e6a6eff488354823e9a8
Toni Helvilä <toni.helvila@ambientia.fi>
2022-12-12 13:35:31 +0200

    HELOSK-232, Uutisartikkeli -sivuille mahdollisuus lisätä tiedostonostoja. Included flexible-content to single view and added small style fixes for media-bank items

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-  @include('partials.content.single-'.get_post_type())
+    @include('partials.content.single-'.get_post_type())
+
+    <div class="single-container">
+      <div class="content-single">
+        @include('components.employers-flexible-content')
+      </div>
+    </div>
   @endwhile
 @endsection
