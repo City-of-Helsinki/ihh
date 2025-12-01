cd91e39d9bc8e5a0b7878a46ddf973b59c511f95
Timo Virtanen <timo.virtanen@hiq.fi>
2025-12-01 10:24:30 +0200

    HELIHHSP-89
    Adjusted max width for text content
    Adjusted SVG arrow position

--- resources/views/flexible/highlighted_content.php
+++ resources/views/flexible/highlighted_content.php
@@ -1,16 +1,15 @@
-<?php if (get_sub_field('content')) : ?>
+<?php if (get_sub_field('content')): ?>
 <?php
-
-// 'green' OR '#83cac6' etc.
-  $bg_raw  = get_sub_field('background_color', false, false);
-  // Returns hex color or empty string
-  $bg_norm = ihh_color_normalize(is_string($bg_raw) ? $bg_raw : '');
-  // Build classes and styles
-  $bg_class = (!$bg_norm && $bg_raw) ? ' background-' . sanitize_html_class($bg_raw) : '';
-  $bg_style = $bg_norm ? ' style="background-color:' . esc_attr($bg_norm) . ';"' : '';
-  ?>
+$bg_raw = get_sub_field('background_color', false, false); // 'green' OR '#83cac6' etc.
+// Returns hex color or empty string
+$bg_norm = ihh_color_normalize(is_string($bg_raw) ? $bg_raw : '');
+$bg_class = !$bg_norm && $bg_raw ? ' background-' . sanitize_html_class($bg_raw) : '';
+$bg_style = $bg_norm ? ' style="background-color:' . esc_attr($bg_norm) . ';"' : '';
+?>
 
 <div class="highlighted-content<?php echo esc_attr($bg_class); ?> ihhce" <?php echo $bg_style; ?>>
-    <?php the_sub_field('content'); ?>
+    <div class="content-wrapper">
+        <?php the_sub_field('content'); ?>
+    </div>
 </div>
 <?php endif; ?>
