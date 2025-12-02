982134218425bd99477887e54dd4e1401f1471bb
Timo Virtanen <timo.virtanen@hiq.fi>
2025-12-02 09:26:19 +0200

    HELIHHSP-99
    Footer settings removed from customizer

--- app/customizer.php
+++ app/customizer.php
@@ -5,138 +5,22 @@ namespace App;
 /**
  * Theme customizer
  */
-add_action( 'customize_register', function ( \WP_Customize_Manager $wp_customize ) {
-
+add_action('customize_register', function (\WP_Customize_Manager $wp_customize) {
     // Remove redundand sections
-    $wp_customize->remove_section( 'colors' );
-    $wp_customize->remove_section( 'custom_css' );
-    $wp_customize->remove_section( 'static_front_page' );
-
-    $wp_customize->add_section( 'ihh_content_settings', [
-        'title'    => __( 'Content defaults', 'ihh' ),
-        'priority' => 200,
-    ] );
-
-    $wp_customize->add_section( 'ihh_footer_settings', [
-        'title'    => __( 'Footer', 'ihh' ),
-        'priority' => 220,
-    ] );
-
-    $wp_customize->add_section( 'ihh_chat_settings', [
-        'title'    => __( 'Chat', 'ihh' ),
-        'priority' => 230,
-    ] );
-
-    /**
-     * Content defaults
-     */
-    $wp_customize->add_setting( 'ihh_default_lift_image' );
-    $wp_customize->add_control( new \WP_Customize_Media_Control( $wp_customize, 'ihh_default_lift_image', [
-        'label'       => pll__( 'Default lift image' ),
-        'section'     => 'ihh_content_settings',
-        'settings'    => 'ihh_default_lift_image',
-        'description' => pll__( 'The default lift image if item does not have one.' ),
-    ] ) );
-
-    /**
-     * Social media
-     */
-    $wp_customize->add_setting( 'ihh_some_twitter' );
-    $wp_customize->add_control( 'ihh_some_twitter', [
-        'type'        => 'url',
-        'section'     => 'ihh_footer_settings',
-        'label'       => 'Twitter',
-        'input_attrs' => [
-            'placeholder' => 'https://twitter.com/pagename',
-        ],
-    ] );
-
-    $wp_customize->add_setting( 'ihh_some_facebook' );
-    $wp_customize->add_control( 'ihh_some_facebook', [
-        'type'        => 'url',
-        'section'     => 'ihh_footer_settings',
-        'label'       => 'Facebook',
-        'input_attrs' => [
-            'placeholder' => 'https://facebook.com/pagename',
-        ],
-    ] );
-
-    $wp_customize->add_setting( 'ihh_some_youtube' );
-    $wp_customize->add_control( 'ihh_some_youtube', [
-        'type'        => 'url',
-        'section'     => 'ihh_footer_settings',
-        'label'       => 'Youtube',
-        'input_attrs' => [
-            'placeholder' => 'https://youtube.com/pagename',
-        ],
-    ] );
-
-    $wp_customize->add_setting( 'ihh_some_linkedin' );
-    $wp_customize->add_control( 'ihh_some_linkedin', [
-        'type'        => 'url',
-        'section'     => 'ihh_footer_settings',
-        'label'       => 'LinkedIn',
-        'input_attrs' => [
-            'placeholder' => 'https://www.linkedin.com/pagename',
-        ],
-    ] );
-
-    $wp_customize->add_setting( 'ihh_footer_image' );
-    $wp_customize->add_control( new \WP_Customize_Media_Control( $wp_customize, 'ihh_footer_image', [
-        'label'       => pll__( 'Footer image' ),
-        'section'     => 'ihh_footer_settings',
-        'settings'    => 'ihh_footer_image',
-        'description' => pll__( 'Footer image' ),
-    ] ) );
-
-    if (function_exists('pll_languages_list')){
-        $translations = pll_languages_list();
-    } else{
-        $translations = array('en');
-    }
-    for( $i=0; $i<count($translations); $i++){
-        $slug = $translations[$i];
-        $wp_customize->add_setting( 'ihh_footer_text_' . $slug );
-
-        $wp_customize->add_control( 'ihh_footer_text_' . $slug, [
-            'type'        => 'textarea',
-            'section'     => 'ihh_footer_settings',
-            'label'       => 'Footer text (' . $slug  . ')',
-            'input_attrs' => [],
-        ] );
-
-        $wp_customize->add_setting( 'ihh_footer_contact_' . $slug );
-        $wp_customize->add_control( 'ihh_footer_contact_' . $slug, [
-            'type'        => 'textarea',
-            'section'     => 'ihh_footer_settings',
-            'label'       => 'Contact text (' . $slug  . ')',
-            'input_attrs' => [],
-        ] );
-    }
-
-
-
-    $wp_customize->add_setting( 'ihh_chat_script' );
-    $wp_customize->add_control( 'ihh_chat_script', [
-        'type'        => 'textarea',
-        'section'     => 'ihh_chat_settings',
-        'label'       => 'Chat script',
-        'input_attrs' => [],
-    ] );
-
-    $wp_customize->add_setting( 'ihh_hide_chat' );
-    $wp_customize->add_control( 'ihh_hide_chat', [
-        'type'        => 'checkbox',
-        'section'     => 'ihh_chat_settings',
-        'label'       => 'Hide Chat',
-        'input_attrs' => [],
-    ] );
-} );
+    $wp_customize->remove_section('colors');
+    $wp_customize->remove_section('custom_css');
+    $wp_customize->remove_section('static_front_page');
+});
 
 /**
  * Customizer JS
  */
-add_action( 'customize_preview_init', function () {
-    wp_enqueue_script( 'ihh/customizer.js', asset_path( 'scripts/customizer.js' ), [ 'customize-preview' ], null,
-        true );
-} );
+add_action('customize_preview_init', function () {
+    wp_enqueue_script(
+        'ihh/customizer.js',
+        asset_path('scripts/customizer.js'),
+        ['customize-preview'],
+        null,
+        true,
+    );
+});
